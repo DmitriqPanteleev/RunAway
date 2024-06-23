f45c630756746762540c4bb56edc6d03831c2139
DmitriqPanteleev <pantele3vd@gmail.com>
2024-06-23 22:21:54 +0400

    UIViewController extension

--- away/Utils/Extensions/UIViewController+Extensions.swift
+++ away/Utils/Extensions/UIViewController+Extensions.swift
@@ -8,5 +8,10 @@
 import UIKit
 
 extension UIViewController {
-    
+    func setupChildViewController(_ childVC: UIViewController, layoutBlock: Closure) {
+        addChild(childVC)
+        self.view.addSubview(childVC.view)
+        layoutBlock()
+        childVC.didMove(toParent: self)
+    }
 }
